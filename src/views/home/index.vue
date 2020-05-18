9e7f725916b3153462f8940eb62666a53c0deaaf
ifer-itcast <yangkangkang@itcast.cn>
2020-05-18 11:24:23 +0800

    处理tabs样式

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="home-container">
-    Home
+    <!-- 搜索栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        class="search-btn"
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
+      >搜索</van-button>
+    </van-nav-bar>
+    <!-- /搜索栏 -->
+    <!-- 频道列表 -->
+    <van-tabs v-model="active" animated swipeable class="channel-tabs">
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <div slot="nav-right" class="placeholder"></div>
+      <div slot="nav-right" class="hamburger-btn">
+        <i class="iconfont icongengduo"></i>
+      </div>
+    </van-tabs>
+    <!-- /频道列表 -->
   </div>
 </template>
 
@@ -10,6 +37,7 @@ export default {
 
   data () {
     return {
+      active: 0
     }
   },
 
@@ -18,5 +46,71 @@ export default {
 </script>
 
 <style scoped lang='less'>
-
+.home-container {
+  /deep/ .van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+  /deep/ .channel-tabs {
+    .van-tabs__wrap {
+      height: 82px;
+    }
+    .van-tab {
+      min-width: 187.5px; // 750 / 4
+      border-right: 1px solid #edeff3;
+      font-size: 30px;
+      color: #777;
+    }
+    .van-tab--active {
+      color: #333;
+    }
+    .van-tabs__nav {
+      padding-bottom: 0;
+    }
+    .van-tabs__line {
+      bottom: 8px;
+      width: 31px !important;
+      height: 6px;
+      background-color: #3296fa;
+    }
+    .placeholder {
+      // 不参与 flex 分配空间的计算
+      flex-shrink: 0;
+      width: 66px;
+      height: 80px;
+    }
+    .hamburger-btn {
+      position: fixed;
+      right: 0;
+      width: 66px;
+      height: 80px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #fff;
+      opacity: .902;
+      .iconfont {
+        font-size: 33px;
+      }
+      &::before {
+        content: "";
+        width: 1px;
+        height: 100%;
+        background-image: url(~@/assets/gradient-gray-line.png);
+        position: absolute;
+        left: 0;
+        background-size: contain;
+      }
+    }
+  }
+}
 </style>
