d8d8d5542a527860444c4190d489a960a1692703
ifer-itcast <yangkangkang@itcast.cn>
2020-05-25 11:06:32 +0800

    搜索记录、提示、结果页面布局

---
+++ src/views/search/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="search-container">
+    <!-- 搜索栏 -->
+    <form action="/">
+      <van-search
+        v-model="searchText"
+        show-action
+        placeholder="请输入搜索关键词"
+        background="#3296fa"
+        @search="onSearch"
+        @cancel="onCancel"
+      />
+    </form>
+    <!-- /搜索栏 -->
+    <!-- 搜索记录 -->
+    <search-history/>
+    <!-- /搜索记录 -->
+    <!-- 搜索提示 -->
+    <search-suggestion></search-suggestion>
+    <!-- /搜索提示 -->
+    <!-- 搜索结果 -->
+    <search-result></search-result>
+    <!-- /搜索结果 -->
+  </div>
+</template>
+
+<script>
+import SearchHistory from './components/search-history'
+import SearchSuggestion from './components/search-suggestion'
+import SearchResult from './components/search-result'
+export default {
+  name: 'SearchIndex',
+  components: {
+    SearchHistory,
+    SearchSuggestion,
+    SearchResult
+  },
+  data () {
+    return {
+      searchText: ''
+    }
+  },
+
+  methods: {
+    onSearch (val) {
+      console.log('x')
+    },
+    onCancel () {
+      this.$router.back()
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.search-container {
+  .van-search__action {
+    color: #fff;
+  }
+}
+</style>
