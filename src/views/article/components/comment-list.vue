0645acae2fe90480ad75feb14796186986cd545d
ifer-itcast <yangkangkang@itcast.cn>
2020-05-28 15:48:45 +0800

    展示文章评论列表

--- src/views/article/components/comment-list.vue
+++ src/views/article/components/comment-list.vue
@@ -4,42 +4,69 @@
       v-model="loading"
       :finished="finished"
       finished-text="没有更多了"
+      :error="error"
+      error-text="加载失败，请点击重试"
       @load="onLoad"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
+      <van-cell
+        v-for="(item, index) in list"
+        :key="index"
+        :title="item.content"
+      />
     </van-list>
   </div>
 </template>
 
 <script>
+import { getComments } from '@/api/comment'
 export default {
   name: 'CommentList',
-
+  components: {},
+  props: {
+    source: {
+      type: [Number, String, Object],
+      required: true
+    }
+  },
   data () {
     return {
       list: [],
       loading: false,
-      finished: false
+      finished: false,
+      offset: null, // 获取下一页数据的标记
+      limit: 10,
+      error: false
     }
   },
 
   methods: {
-    onLoad () {
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-
-        // 加载状态结束
+    // 评论测试：http://localhost:8080/#/article/137825
+    async onLoad () {
+      try {
+        // 1. 请求获取数据
+        const { data } = await getComments({
+          type: 'a', // 评论类型 a-对文章的评论，c-对评论的评论
+          source: this.source, // 文章 id 或评论 id
+          offset: this.offset, // 获取下一页数据的标记
+          limit: this.limit// 每次获取数据数量
+        })
+        // 2. 将数据添加到列表中
+        const { results } = data.data
+        this.list.push(...results)
+        // 3. 将 loading 设置为 false
         this.loading = false
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
+        // 4. 判断是否还有数据
+        if (results.length) {
+          // 4.1 有就更新页码并获取数据
+          this.offset = data.data.last_id
+        } else {
+          // 4.2 没有就将 finished 设置结束
           this.finished = true
         }
-      }, 1000)
+      } catch (err) {
+        this.error = true
+        this.loading = false
+      }
     }
   }
 }
