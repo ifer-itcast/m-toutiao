0fa4f094f91790c6e018878252f9a304ecda8305
ifer-itcast <yangkangkang@itcast.cn>
2020-05-29 16:24:20 +0800

    回复评论布局结构

--- src/views/article/components/comment-reply.vue
+++ src/views/article/components/comment-reply.vue
@@ -9,16 +9,23 @@
         @click="$emit('close')"
       />
     </van-nav-bar>
-    <!-- 当前评论项 -->
-    <comment-item :comment="comment"/>
-    <!-- /当前评论项 -->
-    <!-- 评论回复列表 -->
-    <van-cell title="全部回复"/>
-    <comment-list
-      :source="comment.com_id"
-      type='c'
-    />
-    <!-- /评论回复列表 -->
+    <div class="scroll-wrap">
+      <!-- 当前评论项 -->
+      <comment-item :comment="comment"/>
+      <!-- /当前评论项 -->
+      <!-- 评论回复列表 -->
+      <van-cell title="全部回复"/>
+      <comment-list
+        :source="comment.com_id"
+        type='c'
+      />
+      <!-- /评论回复列表 -->
+    </div>
+    <!-- 回复评论 -->
+    <div class="post-wrap">
+      <van-button class="post-btn" size="small" round>写评论</van-button>
+    </div>
+    <!-- /回复评论 -->
   </div>
 </template>
 
@@ -50,5 +57,27 @@ export default {
 </script>
 
 <style lang='less' scoped>
-
+.scroll-wrap {
+  position: fixed;
+  top: 92px;
+  right: 0;
+  left: 0;
+  bottom: 88px;
+  overflow-y: auto;
+}
+.post-wrap {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 88px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  border-top: 1px solid #d8d8d8;
+  .post-btn {
+    width: 60%;
+  }
+}
 </style>
